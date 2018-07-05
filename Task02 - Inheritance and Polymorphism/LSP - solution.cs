fd5890fac2523fe43ef8d7a9b4123170b2c1eb01
Cebotari Radu <radu.cebotari@outlook.com>
2018-07-05 17:16:50 +0300

    #3 - added LSP principle

---
+++ Task02 - Inheritance and Polymorphism/LSP - solution.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace ConsoleApp
+{
+    interface IPay
+    {
+        bool PayMoney(int ammount);
+    }
+
+    interface IBorrow
+    {
+        void BorrowMoney(int ammount);
+    }
+
+    public class BasicAccount : IPay
+    {
+        int balance;
+        public BasicAccount()
+        {
+            balance = 0;
+        }
+        public bool PayMoney(int ammount)
+        {
+            if (balance < ammount)
+                return false;
+            balance -= ammount;
+            return true;
+        }
+    }
+    public class GoldAccount : IPay, IBorrow
+    {
+        int balance, borrowedMoney;
+        public GoldAccount()
+        {
+            balance = 0;
+            borrowedMoney = 0;
+        }
+        public bool PayMoney(int ammount)
+        {
+            if (balance < ammount)
+                return false;
+            balance -= ammount;
+            return true;
+        }
+        public void BorrowMoney(int ammount)
+        {
+            balance += ammount;
+            borrowedMoney += ammount;
+        }
+    }
+    
+}
---
+++ Task02 - Inheritance and Polymorphism/LSP - violating principle.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace ConsoleApp
+{
+    public abstract class Account
+    {
+        protected int balance;
+        public abstract bool PayMoney(int ammount);
+        public abstract void BorrowMoney(int ammount);
+    }
+
+    public class BasicAccount : Account
+    {
+        public BasicAccount()
+        {
+            balance = 0;
+        }
+        public override bool PayMoney(int ammount)
+        {
+            if (balance - amount < 0)
+                return false;
+            balance -= ammount
+            return true;
+        }
+        public override void BorrowMoney(int ammount)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class GoldAccount : Account
+    {
+        int borrowedAmmount;
+        public GoldAccount()
+        {
+            borrowedAmmount = 0;
+            balance = 0;
+        }
+        public override bool PayMoney(int ammount)
+        {
+            if (balance - ammount < 0)
+                return false;
+            balance -= ammount
+            return true;
+        }
+
+        public override void BorrowMoney(int ammount)
+        {
+            balance += ammount;
+        }
+    }
+}
