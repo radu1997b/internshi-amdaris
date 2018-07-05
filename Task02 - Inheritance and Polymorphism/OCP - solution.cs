1faca8470bf2cb885a42874d375d003a71f26381
Cebotari Radu <radu.cebotari@outlook.com>
2018-07-05 12:50:15 +0300

    OCP example added

---
+++ Task02 - Inheritance and Polymorphism/OCP - solution.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace ConsoleApp
+{
+    interface ISort
+    {
+        void Sort(ref List<int> L);
+    }
+
+    class BubbleSort : ISort
+    {
+        public void Sort(ref List<int> L)
+        {
+            bool ok = false;
+            while (!ok)
+            {
+                ok = true;
+                for (var i = 0; i < L.Count - 1; ++i)
+                    if (L[i] > L[i + 1])
+                    {
+                        int aux = L[i];
+                        L[i] = L[i + 1];
+                        L[i + 1] = aux;
+                        ok = false;
+                    }
+            }
+        }
+    }
+
+    class InjectionSort : ISort
+    {
+        public void Sort(ref List<int> L)
+        {
+            for (var i = 0; i < L.Count; ++i)
+            {
+                int j = i;
+                while (j > 0 && L[j] < L[j - 1])
+                {
+                    int aux = L[j];
+                    L[j] = L[j - 1];
+                    L[j - 1] = aux;
+                    j--;
+                }
+            }
+        }
+    }
+
+    class Multime
+    {
+        ISort algorithm;
+        List<int> L;
+        public Multime(ISort algorithm,List<int> L)
+        {
+            this.algorithm = algorithm;
+            this.L = L;
+        }
+
+        public void ChangeSortingAlgorithm(ISort algorithm)
+        {
+            this.algorithm = algorithm;
+        }
+
+        public void Sort()
+        {
+            algorithm.Sort(ref L);
+        }
+
+        public void Afis()
+        {
+            foreach (var i in L)
+                Write(i.ToString() + " ");
+            WriteLine();
+        }
+    }
+   class Program
+    {
+
+        public static void Main()
+        {
+            Multime M = new Multime(new BubbleSort(), new List<int> { 2, 5, 1, 3, 4 });
+            M.Afis();
+            M.ChangeSortingAlgorithm(new InjectionSort());
+            M.Sort();
+            M.Afis();
+        }
+    }
+}
---
+++ Task02 - Inheritance and Polymorphism/OCP - violating principle.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace ConsoleApp
+{
+
+    public enum SortingAlgorithm { BubbleSort, InjectionSort };
+    public class Multime
+    {
+
+        private List<int> L;
+        private SortingAlgorithm algorithm;
+        public Multime(SortingAlgorithm algorithm,List<int> L)
+        {
+            this.algorithm = algorithm;
+            this.L = L;
+        }
+        private void BubbleSortAlgorithm()
+        {
+            bool ok = false;
+            while (!ok)
+            {
+                ok = true;
+                for (var i = 0; i < L.Count - 1; ++i)
+                    if (L[i] > L[i + 1])
+                    {
+                        int aux = L[i];
+                        L[i] = L[i + 1];
+                        L[i + 1] = aux;
+                        ok = false;
+                    }
+            }
+        }
+        private void InjectionSortAlgorithm()
+        {
+            for (var i = 0; i < L.Count; ++i)
+            {
+                int j = i;
+                while (j > 0 && L[j] < L[j - 1])
+                {
+                    int aux = L[j];
+                    L[j] = L[j - 1];
+                    L[j - 1] = aux;
+                    j--;
+                }
+            }
+        }
+        public void Sort()
+        {
+            switch (algorithm)
+            {
+                case SortingAlgorithm.BubbleSort:
+                    BubbleSortAlgorithm();
+                    break;
+                case SortingAlgorithm.InjectionSort:
+                    InjectionSortAlgorithm();
+                    break;
+            }
+        }
+        public void Afis()
+        {
+            foreach (var i in L)
+                Write(i.ToString() + " ");
+            WriteLine();
+        }
+    }
+    class Program
+    {
+
+        public static void Main()
+        {
+            Multime M = new Multime(SortingAlgorithm.BubbleSort, new List<int> { 2, 5, 4, 6, 10, 9, 3 });
+            M.Sort();
+            M.Afis();
+        }
+    }
+}
