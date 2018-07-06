c32bbd931b4ba679b9bfdc4588710a05444df62d
Cebotari Radu <radu.cebotari@outlook.com>
2018-07-06 10:30:52 +0300

    Hello World program added

---
+++ Task03 - C# Basic/Hello World.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace ConsoleApp
+{
+    
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            WriteLine("Hello World");
+        } 
+    }
+}
