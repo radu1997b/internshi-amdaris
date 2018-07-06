9f02306a3560362a6bd8b693bbf6a25c728d49e3
Cebotari Radu <radu.cebotari@outlook.com>
2018-07-06 11:19:50 +0300

    asfsdsd

---
+++ Task03 - C# Basic/Reference and Value Types.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace ConsoleApp
+{
+    
+    class Car
+    {
+        private String Color;
+        private String Name;
+        private int numOfSeats;
+
+        public Car(String Color,String Name,int numOfSeats)
+        {
+            this.Color = Color;
+            this.Name = Name;
+            this.numOfSeats = numOfSeats;
+        }
+    }
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            // Reference Type
+            var Ford = new Car("Red", "Ford Focus", 5);
+            //Value Type
+            int x = 10;
+            WriteLine(x);
+        } 
+    }
+}
